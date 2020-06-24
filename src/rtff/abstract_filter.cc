4a3858f8f8ecdbdf8a72b9787a327846f6478207
gvne <guillaume.vincke@gmail.com>
2020-06-24 20:52:10 +0200

    Fix the prepare to play function in AbstractFilter

--- src/rtff/abstract_filter.cc
+++ src/rtff/abstract_filter.cc
@@ -5,23 +5,32 @@
 namespace rtff {
 
 class AbstractFilter::Impl : public MixingFilter<1, 1> {
- public:
-  Impl(AbstractFilter* instance) : instance_(instance) {}
-   void ProcessTimeFrequencyBlock(MixingFilter<1, 1>::TimeFrequencyInput input, uint32_t size, MixingFilter<1, 1>::TimeFrequencyOutput* output) {
-     
-     // Process the block
-     instance_->ProcessTransformedBlock(input[0], size);
-     // Copy to the output
-     // TODO: this breaks the overall performance. The copy is useless in N to N filters
-     std::vector<std::complex<float>*> output_data = (*output)[0];
-     for (auto channel_idx = 0; channel_idx < channel_count(); channel_idx++) {
-       auto out_map = Eigen::Map<Eigen::VectorXcf>((*output)[0][channel_idx], size);
-       auto in_map = Eigen::Map<Eigen::VectorXcf>(input[0][channel_idx], size);
-       out_map = in_map;
-     }
-   }
+public:
+  Impl(AbstractFilter *instance) : instance_(instance) {}
+  void
+  ProcessTimeFrequencyBlock(MixingFilter<1, 1>::TimeFrequencyInput input,
+                            uint32_t size,
+                            MixingFilter<1, 1>::TimeFrequencyOutput *output) {
+
+    // Process the block
+    instance_->ProcessTransformedBlock(input[0], size);
+    // Copy to the output
+    // TODO: this breaks the overall performance. The copy is useless in N to N
+    // filters
+    std::vector<std::complex<float> *> output_data = (*output)[0];
+    for (auto channel_idx = 0; channel_idx < channel_count(); channel_idx++) {
+      auto out_map =
+          Eigen::Map<Eigen::VectorXcf>((*output)[0][channel_idx], size);
+      auto in_map = Eigen::Map<Eigen::VectorXcf>(input[0][channel_idx], size);
+      out_map = in_map;
+    }
+  }
+
+protected:
+  void PrepareToPlay() override { instance_->PrepareToPlay(); }
+
 private:
-  AbstractFilter* instance_;
+  AbstractFilter *instance_;
 };
 
 AbstractFilter::AbstractFilter() : impl_(std::make_shared<Impl>(this)) {}
@@ -29,17 +38,17 @@ AbstractFilter::AbstractFilter() : impl_(std::make_shared<Impl>(this)) {}
 AbstractFilter::~AbstractFilter() {}
 
 void AbstractFilter::Init(uint8_t channel_count, uint32_t fft_size,
-                          uint32_t overlap, std::error_code& err) {
+                          uint32_t overlap, std::error_code &err) {
   impl_->Init(channel_count, fft_size, overlap, err);
 }
 
 void AbstractFilter::Init(uint8_t channel_count, uint32_t fft_size,
                           uint32_t overlap, fft_window::Type windows_type,
-                          std::error_code& err) {
+                          std::error_code &err) {
   impl_->Init(channel_count, fft_size, overlap, windows_type, err);
 }
 
-void AbstractFilter::Init(uint8_t channel_count, std::error_code& err) {
+void AbstractFilter::Init(uint8_t channel_count, std::error_code &err) {
   impl_->Init(channel_count, err);
 }
 
@@ -52,25 +61,23 @@ uint8_t AbstractFilter::channel_count() const { return impl_->channel_count(); }
 uint32_t AbstractFilter::window_size() const { return impl_->window_size(); }
 uint32_t AbstractFilter::fft_size() const { return impl_->fft_size(); }
 uint32_t AbstractFilter::overlap() const { return impl_->overlap(); }
-fft_window::Type AbstractFilter::windows_type() const { return impl_->windows_type(); }
+fft_window::Type AbstractFilter::windows_type() const {
+  return impl_->windows_type();
+}
 uint32_t AbstractFilter::hop_size() const { return impl_->hop_size(); }
 
-uint32_t AbstractFilter::FrameLatency() const {
-  return impl_->FrameLatency();
-}
+uint32_t AbstractFilter::FrameLatency() const { return impl_->FrameLatency(); }
 
-void AbstractFilter::ProcessBlock(Waveform* buffer) {
+void AbstractFilter::ProcessBlock(Waveform *buffer) {
   Write(*buffer);
   Read(buffer);
 }
 
-void AbstractFilter::Write(const Waveform& buffer) {
-  impl_->Write(&buffer);
-}
+void AbstractFilter::Write(const Waveform &buffer) { impl_->Write(&buffer); }
 
-void AbstractFilter::Read(Waveform* buffer) {
-  impl_->Read(reinterpret_cast<MixingFilter<1, 1>::Output*>(buffer));
+void AbstractFilter::Read(Waveform *buffer) {
+  impl_->Read(reinterpret_cast<MixingFilter<1, 1>::Output *>(buffer));
 }
 
 void AbstractFilter::PrepareToPlay() {}
-}  // namespace rtff
+} // namespace rtff
